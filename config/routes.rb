a66d78e68fbc71545711ce8455fa71c5ce15a7e8
Juri Matsuda <matsuda.juri@gmail.com>
2017-12-21 17:26:24 +0900

    file uploader etc...

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/info'
+
+  root to: redirect('/ideas')
+  get "page/info"
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
